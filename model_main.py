50353600f00eb894a9f8e2d4cf3858c8cccf5518
teddylew12 <43972226+teddylew12@users.noreply.github.com>
2019-01-31 18:59:33 -0800

    Created main file

---
+++ model_main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Main File for Music_Brain_Project
+
+@author: Ted Lewitt,Ben Hahn, Jack Elliot
+"""
+
+import pandas
+import numpy
+import nibabel as nib
+
+print("Hello World")
