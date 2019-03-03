86a0b8cda9cb248c349544b2d7b61f321caac32b
Ted Lewitt <lewitt@usc.edu>
2019-03-02 16:13:35 -0800

    Added server capabilities

--- model_main.py
+++ model_main.py
@@ -2,23 +2,9 @@
 """
 Main File for Music_Brain_Project
 
-@author: Ted Lewitt,Ben Hahn, Jack Elliot
-"""
-
-"""
-WorkFlow
-
-Thoughts:
-    Use Sphinx to document, make the repos page a sphinx document
-"""
-
-
-
-
- 
-"""
+@author: Ted Lewitt,Ben Hahn, Jack Elliot,Skylar Van Sijil MacMillan
 ------------------------------------
-Imports and Global Variables
+Imports
 ------------------------------------
 """
 
@@ -34,20 +20,20 @@ import nilearn
 import os
 from sklearn.model_selection import train_test_split
 
-
+server=False
+if server:
+    DATA_PATH_FOR_NII=os.path.abspath(~/home/bhan/data/nifti)
+    DATA_PATH_FOR_LABELS=os.path.abspath(~/home/bhan/data/text)
+else:
+    DATA_PATH_FOR_LABELS=r"C:\Users\Ted\Desktop\CAIS_MUSIC_BRAIN\TXT-Files"
+    DATA_PATH_FOR_NII=r"C:\Users\Ted\Desktop\CAIS_MUSIC_BRAIN\NII-Files"
 """
 --------------------------------------
 Data Preprocessing 
 i.e. Everything before model=Sequential()
 --------------------------------------
-"""
-
-
-"""
 Preprocessing the slider data from 10hz to 1hz to match fmri data
 maxpool=1, minpool=2,mean=3,median=4
-
-
 
 
 filename=os.path.join(DATA_PATH_FOR_LABELS,"sub-01_snl_l_enjoy_log.txt")
@@ -65,13 +51,14 @@ niiname=os.path.join(DATA_PATH_FOR_NII,"sub-01_sadln_filtered_func_200hpf_cut20_
 timeSeries=hf.niiToTS(niiname)
 print(timeSeries.shape)
 #print(np.array(timeSeries)[1][1:10])
+  
+#import pdb; pdb.set_trace()
 
+#pd.DataFrame(np.array(train_subset_labels)).to_csv(os.path.join(DATA_PATH_FOR_LABELS,"preprocessed_labels.csv"),header=None,index=None)
+#pd.DataFrame(np.array(train_subset_nii)).to_csv(os.path.join(DATA_PATH_FOR_NII,"preprocessed_nii.csv"),header=None,index=None)
 """
-#Used to calculate output of the LSTM layer
 
 
-DATA_PATH_FOR_LABELS=r"C:\Users\Ted\Desktop\CAIS_MUSIC_BRAIN\TXT-Files"
-DATA_PATH_FOR_NII=r"C:\Users\Ted\Desktop\CAIS_MUSIC_BRAIN\NII-Files"
 #Takes all the names of the label files and preprocesses them into data for the
 #LSTM
 label_array=[]
@@ -84,8 +71,7 @@ label_array=np.array(label_array)
 nii_array=[]
 for f in os.listdir(DATA_PATH_FOR_NII):
     nii_array.append(np.array(hf.niiToTS(os.path.join(DATA_PATH_FOR_NII,f))))
-    
-#import pdb; pdb.set_trace()
+
 
 
 #Global variable for percent to train on, test on and validate on
@@ -100,21 +86,20 @@ train_subset_labels,test_subset_labels,train_subset_nii,test_subset_nii=train_te
 THE MODEL HERSELF (11/10 dont tell my girlfriend)
 --------------------------------------
 """
-MAX_SLIDER_VALUE=495
-model=Sequential()
-
-model.add(LSTM(units=MAX_SLIDER_VALUE, activation=keras.layers.LeakyReLU(alpha=.025),dropout=.08,input_shape=(495,359320)))
-#model.add(Dense(127))
-
-###########################
+#HyperParameters
+MAX_SLIDER_VALUE=127
+EPOCHS=3
+BATCH_SIZE=1
 LOSS='mean_squared_error'
 OPTIMIZER='RMSprop'
+
+
+model=Sequential()
+model.add(LSTM(units=MAX_SLIDER_VALUE, activation=keras.layers.LeakyReLU(alpha=.025),dropout=.08,input_shape=(495,359320)))
+model.add(Dense(units=MAX_SLIDER_VALUE,activation='softmax'))
+
 model.compile(loss=LOSS,optimizer=OPTIMIZER, metrics=['acc','mae'])
-#HyperParameters
-EPOCHS=10
-BATCH_SIZE=1
-
-
+import pdb;pdb.set_trace()
 model.fit(np.array(train_subset_nii),np.array(train_subset_labels),epochs=EPOCHS,batch_size=BATCH_SIZE)
 print(model.summary())
 """
